21117a5007dc064201b0c96f0a86f3794645a096
Parris Greaves <parris.greaves@gmail.com>
2021-10-27 14:39:34 -0400

    add the Day List Item, Day List, and Button components. Also associated stories and test files. Most of the work from the week 7 day 2 assignements.

---
+++ src/components/DayList.js
@@ -0,0 +1,15 @@
+import React from "react";
+import DayListItem from "./DayListItem";
+
+export default function DayList(props) {
+
+  const parsedDays = (props.days).map((day) => (
+    <DayListItem key={day.id} {...day} setDay={props.setDay} />
+  ));
+
+  return (
+    <ul>
+      {parsedDays}
+    </ul>
+  )
+}
---
+++ src/components/DayListItem.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import "./DayListItem.scss";
+import classNames from "classnames";
+
+export default function DayListItem(props) {
+  let dayClass = classNames("day-list__item", { "day-list__item--selected": props.selected, "day-list__item--full": props.spots === 0});
+
+  const formatSpots = function (spots) {
+    if (spots === 0) {
+      return "no spots remaining";
+    } else if (spots === 1) {
+      return "1 spot remaining";
+    } else {
+      return spots + " spots remaining";
+    }
+  };
+
+  return (
+    <li onClick={() => props.setDay(props.name)}>
+      <h2 className={dayClass}>{props.name}</h2>
+      <h3 className={dayClass}>{formatSpots(props.spots)}</h3>
+    </li>
+  );
+}

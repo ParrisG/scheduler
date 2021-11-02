1d26c185aebaf128da6566e7325d1d953363fc9d
Parris Greaves <parris.greaves@gmail.com>
2021-11-02 16:03:10 -0400

    add PropTypes library, checking for proper prop types in the InterviewerList component

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,8 +1,10 @@
 import React from "react";
+import PropTypes from "prop-types";
 import InterviewerListItem from "./InterviewerListItem";
 import "./InterviewerList.scss";
 
-export default function InterviewerList(props) {
+function InterviewerList(props) {
+  
   const parsedInterviewers = (props.interviewers).map((interviewer) => (
     <InterviewerListItem 
       key={interviewer.id} 
@@ -23,3 +25,9 @@ export default function InterviewerList(props) {
 
   );
 }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
+
+export default InterviewerList;
